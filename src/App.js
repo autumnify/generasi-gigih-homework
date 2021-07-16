b2dba3164edf68af303b449269b943baa496aa2a
yuyan07 <wahyudarmawan728@gmail.com>
2021-07-16 15:29:44 +0700

    Create Playlist Bohemian Rhapsody

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { toUpper } from 'lodash';
 import './App.css';
+import Data from './constants/data1';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="playlist-title">Create Playlist</h1>
+      <img src={Data.album.images[1].url} alt={Data.album.id} className="bohemian-image"></img>
+      
+      <div className="description">
+        <h3>{toUpper(Data.album.type)}</h3>
+        <p>{Data.album.name}</p>
+        <p>{toUpper(Data.album.artists[0].name)}</p>
+        <input type="submit" value="Select" className="tombol"></input>
+      </div>
     </div>
   );
 }
